a95520c5e6ecdb21d8c5242947a4536b5fb168e9
Kelly J Andrews <kelly@kellyjandrews.com>
2018-10-01 12:06:27 -0400

    add message API example

--- examples/send-message-translation.js
+++ examples/send-message-translation.js
@@ -1,10 +1,20 @@
 const Nexmo = require('nexmo');
 const request = require('Request');
+const {
+    NEXMO_API_KEY,
+    NEXMO_API_SECRET,
+    MSFT_API_KEY,
+    NEXMO_APPLICATION_ID,
+    NEXMO_APPLICATION_PRIVATE_KEY,
+    FROM_NUMBER,
+    TO_NUMBER } = require('../constants');
 
 const nexmo = new Nexmo({
     apiKey: NEXMO_API_KEY,
-    apiSecret: NEXMO_API_SECRET
-});
+    apiSecret: NEXMO_API_SECRET,
+    applicationId: NEXMO_APPLICATION_ID,
+    privateKey: NEXMO_APPLICATION_PRIVATE_KEY
+})
 
 let options = {
     headers: {
@@ -23,12 +33,16 @@ request.post(`https://api.cognitive.microsofttranslator.com/translate?api-versio
     // full response [{"detectedLanguage":{"language":"en","score":1.0},"translations":[{"text":"Un mensaje de texto enviado mediante la API de SMS de Nexmo","to":"es"}]}]
     let translatedText = JSON.parse(body)[0].translations[0].text;
 
-    let from = FROM_NUMBER;
-    let to = TO_NUMBER;
-
-    nexmo.message.sendSms(from, to, translatedText, (err, res) => {
-        if (err) throw err;
-        console.log(res);
-    });
+    nexmo.channel.send(
+        { "type": "sms", "number": TO_NUMBER },
+        { "type": "sms", "number": FROM_NUMBER },
+        {
+            "content": {
+                "type": "text",
+                "text": translatedText
+            }
+        },
+        (err, data) => { console.log(data); }
+    );
 });
 
